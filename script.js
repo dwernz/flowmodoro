5b4a3fe351c2dc0f46a214c6b0016d05dbe7e89f
Daniel <62781672+dwernz@users.noreply.github.com>
2024-04-12 19:54:51 -0400

    added: previous work periods

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let isRunning = false;
 let startTime = 0;
 let elapsedTime = 0;
 let breakTime = 0;
-let tookBreak = false;
+let workPeriods = [];
 
 const timerElement = document.getElementById("timer");
 const startButton = document.getElementById("start-button");
@@ -10,6 +10,8 @@ const pauseButton = document.getElementById("pause-button");
 const breakInfo = document.getElementById("break-info");
 const breakDuration = document.getElementById("break-duration");
 const startBreakButton = document.getElementById("start-break");
+const previousWorkPeriodsElement = document.getElementById("previous-work-periods");
+const cycleDisplay = document.getElementById("cycle-display");
 
 function updateTimer() {
     const currentTime = Date.now();
@@ -38,6 +40,7 @@ function startTimer() {
         startButton.style.backgroundColor = '#4caf50';
         startButton.innerText = 'Start';
         breakInfo.style.display = "block";
+        workPeriods.push(elapsedTime); // Save work period duration to array
     }
 }
 
@@ -55,6 +58,23 @@ function startBreak() {
             breakTime = 0;
             timerElement.innerText = "00:00:00";
             breakInfo.style.display = "none";
+
+            const totalWorkTime = workPeriods.reduce((acc, time) => acc + time, 0); // Calculate total work time
+
+            let previousWorkPeriodsText = "";
+            for (let i = 0; i < workPeriods.length; i++) {
+                const workPeriodTime = workPeriods[i];
+                const minutes = Math.floor((workPeriodTime / (1000 * 60)) % 60);
+                const seconds = Math.floor((workPeriodTime / 1000) % 60);
+                previousWorkPeriodsText += `Cycle ${i + 1} - ${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}\n`;
+            }
+            if (workPeriods.length > 1) { // Check if there's more than one work period
+                const totalMinutes = Math.floor((totalWorkTime / (1000 * 60)) % 60);
+                const totalSeconds = Math.floor((totalWorkTime / 1000) % 60);
+                previousWorkPeriodsText += `Total: ${totalMinutes.toString().padStart(2, "0")}:${totalSeconds.toString().padStart(2, "0")}`;
+            }
+
+            cycleDisplay.innerText = previousWorkPeriodsText;
         } else {
             const remainingSeconds = Math.floor(remainingBreakTime / 1000);
             const minutes = Math.floor(remainingSeconds / 60);
@@ -62,6 +82,8 @@ function startBreak() {
             breakDuration.innerText = `${minutes.toString().padStart(1, "0")} minutes ${seconds.toString().padStart(2, "0")} seconds`;
         }
     }, 1000);
+
+    previousWorkPeriodsElement.style.display = 'block';
 }
 
 startButton.addEventListener("click", startTimer);
